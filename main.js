ecbd32b30683031d6b27d86dc470dc4ab32ff252
Patrick <patrick.hurley@codeimmersives.com>
2020-02-11 12:39:53 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function images (id, url){
+const target = document.querySelector('#' + id)
+target.src = url
+}
+images ('image-1', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FD3m66mDjWZ5EA%2Fgiphy.gif&f=1&nofb=1')
+
+
+
+
+
+
+
+
+// const newLi = document.createElement('li');
+// newLi.innerText= 'Hello'
+// const list = document.querySelector('ul');
+// list.appendChild(newLi);
+
+
+// function boderize(id, color){
+//   const target = document.querySelector('#' + id)
+//   target.style.border = '5px solid' + color;
+// }
+// borderize ('item-2', 'green')
+
+// ol.style.backgroundImage = "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FD3m66mDjWZ5EA%2Fgiphy.gif&f=1&nofb=1')"
