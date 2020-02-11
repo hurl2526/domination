c3657686587465061c6e4d119dca29d6f683f088
Patrick <patrick.hurley@codeimmersives.com>
2020-02-11 13:05:40 -0500

    2 down

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ target.src = url
 }
 images ('image-1', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FD3m66mDjWZ5EA%2Fgiphy.gif&f=1&nofb=1')
 
+images ('image-2', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2F37tuh92v7fylxXhd4A%2Fgiphy.gif&f=1&nofb=1')
+
+images ('image-3', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F68.media.tumblr.com%2Fae3177ae4b4f74fad02a9b2dbd825543%2Ftumblr_osjvtgUm6Q1w43wzfo1_400.gif&f=1&nofb=1')
+
+function slash (){
+  return document.querySelector('#lines'
+  ).style.textDecoration = 'line-through'
+}
+slash();  
+
 
 
 
